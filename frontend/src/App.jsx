ac619f1e26acbe6dfc741543249499b7af0c4483
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-01 02:18:12 +0800

    changed bg colour for reviews

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => (
     <section className="padding"><SuperQuality /></section>
     <section className="padding-x py-10"><Services /></section>
     <section className="padding"><SpecialOffer /></section>
-    <section className="padding bg-pale-blue"> <CustomerReviews /></section>
+    <section className="padding bg-pink-100"> <CustomerReviews /></section>
     <section className="padding-x sm:py-32 py-16 w-full"><Subscribe /></section>
     <section className="padding-x padding-t pb-8 bg-black"><Footer /></section>
   </main>
