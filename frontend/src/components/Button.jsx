dcb026b140c8d12e1cce01c5ca1a5293551c5f35
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-01 02:38:41 +0800

    subscribe section

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, fullWidth }) => {
   return (
-    <button className="flex font-serif tracking-wider justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-pink-100 rounded-full border-gray-100">
+    <button
+      className={`flex font-serif tracking-wider justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-pink-100 rounded-full ${
+        fullWidth && "w-full"
+      } border-gray-100`}
+    >
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5 "
-      />
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5 "
+        />
+      )}
     </button>
   );
 };
