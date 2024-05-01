b0607a4fb5dbc03a2e83c90f897ace95c52fc123
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-01 23:22:22 +0800

    footer component

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,5 @@
 import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
+
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
@@ -35,12 +36,12 @@ export const footerLinks = [
   {
       title: "Products",
       links: [
-          { name: "Air Force 1", link: "/" },
-          { name: "Air Max 1", link: "/" },
-          { name: "Air Jordan 1", link: "/" },
-          { name: "Air Force 2", link: "/" },
-          { name: "Nike Waffle Racer", link: "/" },
-          { name: "Nike Cortez", link: "/" },
+          { name: "Roses", link: "/" },
+          { name: "Lilies", link: "/" },
+          { name: "Tulips", link: "/" },
+          { name: "Begonias", link: "/" },
+          { name: "Sunflowers", link: "/" },
+          { name: "Baby Breaths", link: "/" },
       ],
   },
   {
@@ -56,8 +57,8 @@ export const footerLinks = [
   {
       title: "Get in touch",
       links: [
-          { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-          { name: "+92554862354", link: "tel:+92554862354" },
+          { name: "hello@pb.com", link: "mailto:hello@pb.com" },
+          { name: "+65 6123 4567", link: "tel:+6561234567" },
       ],
   },
 ];
--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -1,8 +1,61 @@
+import { copyrightSign } from "../assets/icons";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <h4 className="text-white font-serif text-2xl tracking-wider leading-normal font-medium mb-4">
+            Check Out Our Social Media
+          </h4>
+          <div className="flex items-center gap-5 mt-4">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
 
-export default Footer
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-serif text-2xl tracking-wider leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    key={link.name}
+                    className="mt-3 text-white-400 font-serif tracking-wider leading-normal text-base hover:text-slate-gray"
+                  >
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center ">
+        <div className="flex flex-1 justify-start items-center gap-2 font-serif tracking-wider cursor-pointer ">
+          <img
+            src={copyrightSign}
+            alt="copyright sign"
+            width={20}
+            height={20}
+            className="rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-serif tracking-wider cursor-pointer">
+          Terms & conditions.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
