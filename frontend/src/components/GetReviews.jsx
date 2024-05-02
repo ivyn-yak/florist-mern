67eb501d7f0a752616c23523f39086f688af5be6
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-02 16:34:06 +0800

    linked reviews db and frontend

--- frontend/src/components/GetReviews.jsx
+++ frontend/src/components/GetReviews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {star} from "../assets/icons"
 
 const GetReviews = () => {
     const [reviews, setReviews] = useState([]);
@@ -8,7 +9,6 @@ const GetReviews = () => {
     const response = await axios.get("http://localhost:5001/reviews")
     setReviews(response.data.data)
     console.log(response)
-    console.log(reviews)
   }
 
   useEffect(()=> {
@@ -17,15 +17,15 @@ const GetReviews = () => {
 
   
   return (
-    <div>
+    <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
         {reviews.map((review) => (
-            <div key={review.customerName} className="flex justify-center items-center flex-col"> 
+            <div key={review.name} className="flex justify-center items-center flex-col"> 
                 <img 
                 src={review.imgURL}
-                alt={review.customerName}
+                alt={review.name}
                 className="rounded-full object-cover w-[120px] h-[120px]"
                 />
-                <p className="mt-6 max-w-sm text-center info-text">{review.feedback}</p>
+                <p className="mt-6 max-w-sm text-center info-text">{review.description}</p>
                 <div className="flex mt-3 justify-center items-center gap-2.5">
                     <img 
                     src={star}
@@ -35,7 +35,7 @@ const GetReviews = () => {
                     />
                     <p className="text-xl font-serif tracking-wider text-slate-gray">{review.rating}</p>
                 </div>
-                <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{review.customerName}</h3>
+                <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{review.name}</h3>
             </div>
         ))}
     </div>
