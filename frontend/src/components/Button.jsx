121c4a72de7344bc2c8c30456052dfff35eeecdd
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-03 17:06:30 +0800

    button: hover

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,10 +3,10 @@ const Button = ({ label, iconURL, fullWidth, backgroundColor, textColor, borderC
     <button
       className={`flex font-serif tracking-wider justify-center items-center gap-2 px-7 py-4 border text-lg leading-none
       ${backgroundColor 
-        ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-pink-100"
+        ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-pink-100  hover:bg-pink-200  hover:border-gray-300  hover: border-2 transition-colors duration-200"
       } rounded-full ${
         fullWidth && "w-full"
-      } border-gray-100`}
+      } border-gray-100 transition-transform duration-200 hover:scale-105`}
     >
       {label}
 
