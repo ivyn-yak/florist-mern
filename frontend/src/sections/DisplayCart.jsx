8b96b2990e7ab304ccd47a37789075876e3cf4a4
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-05 18:41:09 +0800

    get cart from db

--- frontend/src/sections/DisplayCart.jsx
+++ frontend/src/sections/DisplayCart.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useCart } from "../components/CartContext";
+import { getCart } from "../constants/api";
 
 const DisplayCart = () => {
-  const { cartProducts, setCartProducts } = useCart();
-  console.log(cartProducts);
+  const [cart, setCart] = useState([]);
+
+  const userId = "2";
+
+  const getUserCart = async ({ userId }) => {
+    const response = await getCart({ userId });
+
+    if (response.length == 0) {
+      console.log("cart is empty");
+    } else {
+      setCart(response[0].cart);
+      console.log(response[0].cart);
+    }
+  };
+
+  useEffect(() => {
+    getUserCart({ userId });
+  }, []);
 
   return (
     <section className="max-container mx-auto px-4 mt-16">
@@ -12,7 +30,13 @@ const DisplayCart = () => {
           <h2 className="font-palanquin text-3xl capitalize font-bold lg:max-w-lg">
             Cart
           </h2>
-          <p className="mt-3">{cartProducts[0]}</p>
+          {cart.map((product) => (
+            <div>
+              <p className="mt-3">{product.id}</p>
+              <p className="mt-3">{product.quantity}</p>
+              <p className="mt-3">{product.message + "heh"}</p>
+            </div>
+          ))}
         </div>
 
         <div className=" rounded-xl bg-stone-100 p-[30px] col-span-2">
