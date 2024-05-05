c5373d0794a90f4097138bc16d1d3e0a16a1b58d
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-05 15:10:51 +0800

    fixed counter bug

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,18 +1,18 @@
-const Counter = ({quantity, setQuantity}) => {
+const Counter = ({ quantity, setQuantity }) => {
+  const increment = () => {
+    setQuantity((prevQty) => prevQty + 1);
+  };
 
-    const increment = () => {
-        setQuantity(prevQty => prevQty + 1);
-      };
-    
-    const decrement = () => {
+  const decrement = () => {
     if (quantity > 1) {
-        setQuantity(prevQty => prevQty - 1);
+      setQuantity((prevQty) => prevQty - 1);
     }
-    };
+  };
 
   return (
     <div className="flex justify-start items-center mt-3">
       <button
+        type="button"
         onClick={decrement}
         className="bg-gray-100  text-gray-700 font-bold w-8 h-8 justify-center items-center flex rounded-full focus:outline-none transition duration-300 ease-in-out hover:bg-gray-200"
       >
@@ -22,13 +22,14 @@ const Counter = ({quantity, setQuantity}) => {
         {quantity}
       </span>
       <button
+        type="button"
         onClick={increment}
         className="bg-gray-100  text-gray-700 font-bold w-8 h-8 justify-center items-center flex rounded-full focus:outline-none transition duration-300 ease-in-out hover:bg-gray-200"
       >
         +
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
