ddea45ccdfdd756a50f43ddf975d5053d31794ee
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-05-15 23:43:15 +0800

    setup .env

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,27 @@
-import express, { request, response } from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import express from "express";
+// import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import cors from "cors"
 import { flowersRoute, reviewsRoute, cartRoute } from "./routes/index.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors())
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type"]
-// }))
 app.use(express.json());
 
 app.use("/flowers", flowersRoute)
 app.use("/reviews", reviewsRoute)
 app.use("/cart", cartRoute)
 
-
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.DBURL)
   .then(() => {
     console.log("App connected to DB");
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
